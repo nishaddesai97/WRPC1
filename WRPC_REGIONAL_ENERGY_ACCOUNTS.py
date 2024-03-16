4c0628db86ee0adced68b8fe77512e7c3f3f00b2
Pradip Kanzariya <133103000+PradipKanzariya@users.noreply.github.com>
2024-03-16 18:50:31 +0530

    Add files via upload

--- WRPC_REGIONAL_ENERGY_ACCOUNTS.py
+++ WRPC_REGIONAL_ENERGY_ACCOUNTS.py
@@ -7,15 +7,20 @@ import pdfplumber  # For PDF processing
 import os
 import io
 
-def create_file(df, sheet_name):
+def create_file(df, sheet_name, pdf_title):
+
     filename = f"Extracted Data_WRPC_SRPC_{datetime.now().strftime('%d-%m-%Y')}.xlsx"
 
     # Check file existence
     if not os.path.exists(filename):
         wb = Workbook()
-        wb.save(filename)
     else:
         wb = load_workbook(filename)
+
+    # Remove the default sheet created by openpyxl
+    if 'Sheet' in wb.sheetnames:
+        default_sheet = wb['Sheet']
+        wb.remove(default_sheet)
 
     # Check if sheet exists
     if sheet_name not in wb.sheetnames:
@@ -29,7 +34,7 @@ def create_file(df, sheet_name):
     # Append data to the worksheet
     for index, row in df.iterrows():  # Iterate over DataFrame rows
         # Split the 'PDF URL' column into two separate columns for hyperlink function
-        row["PDF URL"] = row["PDF URL"].split(", ")[1].strip(')')
+        row["PDF URL"] = pdf_title
         row_list = row.to_list()  # Convert DataFrame row to a list
         ws2.append(row_list)  # Append the row to the worksheet
 
@@ -70,8 +75,8 @@ def search_text_in_pdf(title, url, search_text):
     return found_row
 
 # Function to create clickable links in Excel
-def create_hyperlink(url, title):
-    return f'=HYPERLINK("{url}", "{title}")'
+def create_hyperlink(url, display_text):
+    return f'=HYPERLINK("{url}", "{display_text}")'
 
 # Function to convert extracted row to DataFrame
 def row_to_dataframe(row, title, url, year):
@@ -79,11 +84,10 @@ def row_to_dataframe(row, title, url, year):
         return None
 
     # Split the row into columns based on whitespace
-    columns = row.split()  # Change from row[0].split() to row.split()
+    columns = row.split()
 
     # Verify if the number of columns matches the expected number
     if len(columns) % 4 != 0:
-        print(f"Invalid row data: {row}")
         return None
 
     # Group the columns into chunks of 4 to represent each row of data
@@ -98,16 +102,12 @@ def row_to_dataframe(row, title, url, year):
 
     return df
 
-
 # Function to perform search on multiple PDF URLs and append results into one DataFrame
 def search_text_in_multiple_pdfs(pdf_links, search_text, year):
     all_rows = []
     for title, url in pdf_links:
-        print(f"Searching {search_text} in {url}")
-
         found_row = search_text_in_pdf(title, url, search_text)
         if found_row:
-            print("\t>> Found", found_row)
             all_rows.append((found_row, title, url))
 
     df = pd.concat([row_to_dataframe(row, title, url, year) for row, title, url in all_rows], ignore_index=True)
@@ -118,6 +118,8 @@ def extract_data(year, title_filter):
     wrpc_base_url = "https://www.wrpc.gov.in"
     REA_link = f"{wrpc_base_url}/assets/data/REA_{year}.txt"
     response = requests.get(REA_link)
+    pdf_title = ""
+    pdf_url = ""
     if response.status_code == 200:
         rea_data = response.text.split("\n")
         pdf_links = []
@@ -127,21 +129,19 @@ def extract_data(year, title_filter):
                 if len(data_parts) >= 3:
                     month_year = data_parts[0].strip()
                     title = data_parts[0].strip() + ", " + data_parts[1].strip()
-                    print("title", title)
-                    print("title_filter.lower()", title_filter.lower())
+                    pdf_title = title
 
                     link = wrpc_base_url + "/" + data_parts[2].strip()
+                    pdf_url = link
                     if title_filter.lower() in title.lower():
-                        print("matched")
                         pdf_links.append((title, link))
-        for index, (title, link) in enumerate(pdf_links):
-            print(f"{index}, \"{title}\", \"{link}\"")
         
         # Search for the text in the multiple PDFs and append the results into one DataFrame
         df = search_text_in_multiple_pdfs(pdf_links, "Arinsun_RUMS", year)  # You can adjust the search text as needed
         st.write(df)
         sheet_name = 'WRPC_Monthly Scheduled Revenue'
-        create_file(df, sheet_name)
+        hyper_link = create_hyperlink(pdf_url, pdf_title)
+        create_file(df, sheet_name, hyper_link)
 
 if __name__ == '__main__':
     # REGIONAL ENERGY ACCOUNTS
@@ -159,4 +159,4 @@ if __name__ == '__main__':
     if st.button('Extract Data'):
         extract_data(selected_year, selected_month)
         st.write(f"Data extracted for: {selected_month}, {selected_year}")
-        print('Done')
+        print("Exit(0)")
