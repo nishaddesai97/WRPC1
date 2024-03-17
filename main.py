dd16853d3765a680ba19e3dbc46e589b2d33df19
mishrasharad332 <163420422+mishrasharad332@users.noreply.github.com>
2024-03-17 16:32:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -37,4 +37,5 @@ if __name__ == '__main__':
             if value:
                 function_mapping[label](selected_year, selected_month)  # Call the corresponding function based on the selected option
         
-        download_file()
+        if st.button('Download Data'):
+            download_file()
