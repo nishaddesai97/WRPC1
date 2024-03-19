ebddbef749026ca33fc922a03aaf337ca6794cee
mishrasharad332 <163420422+mishrasharad332@users.noreply.github.com>
2024-03-19 19:20:18 +0530

    Update srpc_dsm.py

--- srpc_dsm.py
+++ srpc_dsm.py
@@ -1,48 +1,96 @@
+import xml.etree.ElementTree as ET
+from datetime import datetime
 import streamlit as st
+from io import BytesIO
+import pandas as pd
+import pdfplumber
 import requests
 import urllib3
-import xml.etree.ElementTree as ET
 import re
 
-# Disable SSL certificate verification warning
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)  # Disable SSL certificate verification warning
+def get_pdf(url):
+    try:
+        response = requests.get(url, verify=False)
+        response.raise_for_status()  # Raise exception for HTTP errors
+        return response.content
+    except requests.RequestException as e:
+        print(f"Error fetching PDF: {e}")
+        return None
 
-# Fetching data from the provided URL
-url = "https://www.srpc.kar.nic.in/html/xml-search/data/commercial.xml?cache="
-response = requests.get(url, verify=False)
-xml_data = response.content
+def search_and_extract(pdf_bytes, search_word):
+    tables = []
+    try:
+        with pdfplumber.open(BytesIO(pdf_bytes)) as pdf:
+            escaped_word = re.escape(search_word)
+            pattern = re.compile(fr"\b{escaped_word}\b", re.IGNORECASE)
+            for page_number, page in enumerate(pdf.pages, start=1):
+                text = page.extract_text()
+                if pattern.search(text):
+                    tables_ = page.extract_tables()
+                    for table in tables_:
+                        tables.append(table)
+    except Exception as e:
+        print(f"Error extracting tables: {e}")
+    return tables
 
-# Parsing the XML data
-root = ET.fromstring(xml_data)
+def fetch_data(selected_year, selected_month):
+    st.warning("Please select the week for which you'd like to fetch data, then click 'Continue' below.")
+    # Fetching data from the provided URL
+    url = "https://www.srpc.kar.nic.in/html/xml-search/data/commercial.xml?cache="
+    response = requests.get(url, verify=False)
+    xml_data = response.content
+    root = ET.fromstring(xml_data)  # Parsing the XML data
+    # Store the selected PDF URLs
+    selected_urls = []
 
-# Extracting relevant information
-documents = {}
-for document in root.findall('document'):
-    doc_type = document.find('type').text
-    period = document.find('period').text
-    url1_element = document.find('url1')
-    url1 = url1_element.text if url1_element is not None else None
-    if url1 is not None:
-        # Extract year from the period using regex
-        match_year = re.search(r'(\d{4})-(\d{2})', doc_type)  # Match year in the format YYYY-YY
-        match_month = re.search(r'(\w{4})', period) # Match month in the format YYYY-MMM
-        if match_year and match_month:
-            year = match_year.group(1)  # Taking the first year mentioned
-            month = match_month.group(1) # Taking the first month mentioned
-            documents.setdefault(year, {}).setdefault(month, []).append((period, url1))
+    # Extracting relevant information
+    for document in root.findall('document'):
+        period = document.find('period').text
+        urls_found = False     # Flag to track if any URLs are found
+        if selected_month.lower()[:3] in period.lower() and selected_year in period:
+            for i in range(1, 7):  # Assuming URLs are numbered from 1 to 6
+                url_tag = document.find(f'url{i}')
+                if url_tag is not None and url_tag.text.endswith('.pdf') and 'dsm' in url_tag.text.lower():
+                    urls_found = True  # Set flag to True if URLs are found
+                    pdf_url = url_tag.text
+                    title = period
+                    # Create a checkbox for each PDF URL title
+                    checkbox = st.checkbox(title)
+                    # If checkbox is checked, add PDF URL to selected_urls list
+                    if checkbox:
+                        selected_urls.append(pdf_url)
+        
+            # if not urls_found:
+            #     st.error("No data were found for the selected period.")
+
+    if st.button('Continue'):
+        if selected_urls:
+            # fetch_text(selected_urls)
+            for url in selected_urls:
+                # st.write(f"Selected PDF URL: {url}")
+                pdf_bytes = get_pdf(url)
+                if pdf_bytes:
+                    # search_terms =["SPRNG,NPKUNTA", "SPRNG,PUGULUR","Fortum Solar,PAVAGADA"]
+                    search_term = "SPRNG,NPKUNTA"
+                    pages = search_and_extract(pdf_bytes, search_term)
+                    for page in pages:
+                        # Convert tables to DataFrames
+                        df = pd.DataFrame(page)
+                        # Print extracted table with better formatting
+                        pd.set_option('display.max_columns', None)
+                        st.write(df)
+            st.success("Extracted SRPC WA DSM✨")
+        else:
+            st.error("Please select at least one URL before continuing.")
 
 # Streamlit app
-st.title('SRPC DSM Documents')
+st.title('SRPC WA DSM')
+years = ['2024', '2023', '2022', '2021', '2020', '2019', '2018', '2017', '2016', '2015', '2014', '2013', '2012', '2011', '2010']
+# Create dropdown widgets for selecting the year and title filter
+selected_year = st.selectbox('Select a Year:', years)
+months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+selected_month = st.selectbox("Select a month", options=months, index=2, format_func=lambda x: x.title())
 
-# Get unique years from the documents
-years = sorted(documents.keys(), reverse=True)
-selected_year = st.selectbox('Select Year', years)
-
-if selected_year:
-    months = sorted(documents[selected_year].keys())
-    selected_month = st.selectbox('Select Month', months)
-
-    if selected_month:
-        st.subheader(f'Documents for {selected_month} {selected_year}')
-        for period, url1 in documents[selected_year][selected_month]:
-            checkbox_value = st.checkbox(label=f"[{period}]({url1})", value=False)
+# Call fetch_data function
+fetch_data(selected_year, selected_month)
