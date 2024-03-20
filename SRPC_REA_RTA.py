6f04d76f9b8d1122573b2bb3abacca703dd26c3a
Pradip Kanzariya <133103000+PradipKanzariya@users.noreply.github.com>
2024-03-20 17:15:56 +0530

    Add files via upload

--- SRPC_REA_RTA.py
+++ SRPC_REA_RTA.py
@@ -8,6 +8,8 @@ import urllib3
 import os
 import re
 import fitz  # PyMuPDF library for PDF parsing
+import pdfplumber
+import io
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
@@ -26,21 +28,40 @@ def fetch_pdf_urls(month, year, doc_type):
         month_locations[url] = ml
   return pdf_urls, month_locations
 
-def extract_text_from_pdf(pdf_content):
-  with fitz.open("pdf", pdf_content) as doc:
-    text = ""
-    for page in doc:
-      text += page.get_text()
-  return text
+# def extract_text_from_pdf(pdf_content):
+#   with fitz.open("pdf", pdf_content) as doc:
+#     text = ""
+#     for page in doc:
+#       text += page.get_text()
 
-def find_table(text):
-  table_pattern_4col = r'Entity\s+Total\s+Energy\s+Schedule\s+Total\s+Actual\s+data\s+Net\s+Deviation\s+for\s+the\s+purpose\s+of\s+REC'
-  if re.search(table_pattern_4col, text, re.MULTILINE):
-    return 4
-  table_pattern_2col = r'Entity[\s\n]*Total Energy(?: Schedule)?'
-  if re.search(table_pattern_2col, text, re.MULTILINE):
-    return 2
-  return 0
+#       with open("test1.txt", "w", encoding="utf-8") as file:
+#         file.write(text)
+#   return text
+
+def extract_text_from_pdf(pdf_content):
+    text = ""
+    with pdfplumber.open(io.BytesIO(pdf_content)) as pdf:
+        for page in pdf.pages:
+            text += page.extract_text()
+
+    with open("test2.txt", "w", encoding="utf-8") as file:
+        file.write(text)
+    return text
+
+def find_table(text, search_terms):
+      for st in search_terms:
+        pattern = re.compile(r'(^.*?\b' + re.escape(st) + r'\b.*?$)', re.IGNORECASE | re.MULTILINE)
+        matches = pattern.findall(text)
+        for match in matches:
+            match_values = match.split()
+            if len(match_values) == 2:
+                return 2
+            elif len(match_values) == 4:
+                return 4
+            else:
+                # Handle other cases as needed
+                return None
+
 
 def extract_data(text, num_columns, search_terms):
   data = []
@@ -53,11 +74,14 @@ def extract_data(text, num_columns, search_terms):
   for entity in search_terms:
     if entity in processed_entities:
       continue
-    if num_columns == 2:
-      entity_pattern = f'(?m)^{entity}\n(.*?)\n'
-    elif num_columns == 4:
-      entity_pattern = f'(?m)^{entity}\n(.*?)\n(.*?)\n(.*?)\n'
 
+    if num_columns == 2:
+      # entity_pattern = f'(?m)^{entity}\n(.*?)\n'
+      entity_pattern = f'(?m)^{entity}\s+(\d+)\s+'
+    elif num_columns == 4:
+      # entity_pattern = f'(?m)^{entity}\n(.*?)\n(.*?)\n(.*?)\n'
+      entity_pattern = f'(?m)^{entity}\s+(.*?)\s+(.*?)\s+(.*?)\s+'
+      
     entity_match = re.search(entity_pattern, text, re.MULTILINE)
     if entity_match:
       values = [entity]
@@ -66,8 +90,7 @@ def extract_data(text, num_columns, search_terms):
       if num_columns == 2:
         value_lines += ("N/A", ) * 2  # Convert list to tuple here
 
-      values.extend(value_lines[:num_columns -
-                                1])  # Extracting data based on num_columns
+      values.extend(value_lines[:num_columns -1])  # Extracting data based on num_columns
       # if num_columns == 2 then extend with empty two "N/A" values"
       if num_columns == 2:
         values.extend(["N/A", "N/A"])
@@ -82,103 +105,113 @@ def extract_data(text, num_columns, search_terms):
       "Entity", "Total Energy Schedule", "Total Actual data",
       "Net Deviation for the purpose of REC"
   ]
+  print(f"data = {data}")
   d = pd.DataFrame(data, columns=columns)
   print("d", d)
   return d, date_str
 
-
 def fetch_data(year, month):
-  st.write("Extracting SRPC_REA")
-  doc_type = "REA"
-  # month = "jan"
-  # year = "2024"
-  search_terms = ["SPRNG,NPKUNTA", "Fortum Solar,PAVAGADA","SPRNG,PUGULUR"]  # entity names
-  solar_entities = {"SPRNG,NPKUNTA", "Fortum Solar,PAVAGADA"}
-  non_solar_entities = {"SPRNG,PUGULUR"}
+      st.write("Extracting SRPC_REA")
+      doc_type = "REA"
+      # month = "jan"
+      # year = "2024"
+      search_terms = ["SPRNG,NPKUNTA", "Fortum Solar,PAVAGADA","SPRNG,PUGULUR"]  # entity names
+      solar_entities = {"SPRNG,NPKUNTA", "Fortum Solar,PAVAGADA"}
+      non_solar_entities = {"SPRNG,PUGULUR"}
 
-  date_ = ""
+      date_ = ""
 
-  # Fetch PDF URLs
-  pdf_urls, month_locations = fetch_pdf_urls(month, year, doc_type)
-  solar_data, non_solar_data = [], []
+      # Fetch PDF URLs
+      pdf_urls, month_locations = fetch_pdf_urls(month, year, doc_type)
+      solar_data, non_solar_data = [], []
 
-  for pdf_url, month_location in zip(pdf_urls, month_locations.values()):
-    with requests.get(pdf_url, verify=False) as response:
-      text = extract_text_from_pdf(response.content)
-      num_columns = find_table(text)
-      if num_columns:
-        data, date_str = extract_data(text, num_columns, search_terms)
-        date_ = date_str
-        if data is not None:
-          for row in data.values:
-            entity = row[0]
-            if entity in solar_entities:
-              solar_data.append((*row, month_location, pdf_url))
-            elif entity in non_solar_entities:
-              non_solar_data.append((*row, month_location, pdf_url))
-            else:
-              print(f"Entity '{entity}' not found in solar_entities or non_solar_entities sets.")
+      for pdf_url, month_location in zip(pdf_urls, month_locations.values()):
+          with requests.get(pdf_url, verify=False) as response:
+              text = extract_text_from_pdf(response.content)
+              # print(f"text = {text}")
+              num_columns = find_table(text, search_terms)
+              print(f"num_columns = {num_columns}")
+              if num_columns:
+                  data, date_str = extract_data(text, num_columns, search_terms)
+                  date_ = date_str
+                  if data is not None:
+                      for row in data.values:
+                          # print(f"row = {row}")
+                          entity = row[0]
+                          if entity in solar_entities:
+                              solar_data.append((*row, month_location, pdf_url))
+                          elif entity in non_solar_entities:
+                              non_solar_data.append((*row, month_location, pdf_url))
+                          else:
+                              print(f"Entity '{entity}' not found in solar_entities or non_solar_entities sets.")
 
-  # Define column names
-  solar_columns = [
-      "Entity", "Total Energy Schedule", "Total Actual data",
-      "Net Deviation for the purpose of REC", "Month Location", "PDF URL"
-  ]
-  non_solar_columns = [
-      "Entity", "Total Energy Schedule", "Total Actual data",
-      "Net Deviation for the purpose of REC", "Month Location", "PDF URL"
-  ]
+      # if not solar_data and not non_solar_data:
+      #     print("No data found.")
+      #     return
+      
+      if solar_data and non_solar_data:
+        # Define column names
+        solar_columns = [
+            "Entity", "Total Energy Schedule", "Total Actual data",
+            "Net Deviation for the purpose of REC", "Month Location", "PDF URL"
+        ]
+        non_solar_columns = [
+            "Entity", "Total Energy Schedule", "Total Actual data",
+            "Net Deviation for the purpose of REC", "Month Location", "PDF URL"
+        ]
 
-  # Create DataFrames
-  solar_df = pd.DataFrame(solar_data, columns=solar_columns)
-  non_solar_df = pd.DataFrame(non_solar_data, columns=non_solar_columns)
+        # Create DataFrames
+        solar_df = pd.DataFrame(solar_data, columns=solar_columns)
+        non_solar_df = pd.DataFrame(non_solar_data, columns=non_solar_columns)
 
-  # Save DataFrames as CSV files
-  # solar_df.to_csv("solar_data.csv", index=False)
-  # non_solar_df.to_csv("non_solar_data.csv", index=False)
+        # Save DataFrames as CSV files
+        # solar_df.to_csv("solar_data.csv", index=False)
+        # non_solar_df.to_csv("non_solar_data.csv", index=False)
 
-  print("Actual Meter Reading Available Upto :", date_)
-  filename = f"Extracted Data_WRPC_SRPC_{datetime.now().strftime('%d-%m-%Y')}.xlsx"
-  sheet_name = "SRPC_REA"
+        print("Actual Meter Reading Available Upto :", date_)
+        filename = f"Extracted Data_WRPC_SRPC_{datetime.now().strftime('%d-%m-%Y')}.xlsx"
+        sheet_name = "SRPC_REA"
 
-  # Check file existence
-  if not os.path.exists(filename):
-      wb = Workbook()
-      wb.save(filename)
-  else:
-      wb = load_workbook(filename)
+        # Check file existence
+        if not os.path.exists(filename):
+            wb = Workbook()
+            wb.save(filename)
+        else:
+            wb = load_workbook(filename)
 
-  # Check if sheet exists
-  if sheet_name not in wb.sheetnames:
-      wb.create_sheet(title=sheet_name)
-  ws2 = wb[sheet_name]
+        # Check if sheet exists
+        if sheet_name not in wb.sheetnames:
+            wb.create_sheet(title=sheet_name)
+        ws2 = wb[sheet_name]
 
-  ws2.cell(row=1, column=1, value="Actual Meter Reading Available Upto :")
-  ws2.cell(row=1, column=2, value=date_)
+        ws2.cell(row=1, column=1, value="Actual Meter Reading Available Upto :")
+        ws2.cell(row=1, column=2, value=date_)
 
-  # Write headers for solar_df
-  solar_headers = solar_df.columns
-  for col_num, header in enumerate(solar_headers, start=1):
-      cell = ws2.cell(row=2, column=col_num, value=header)
-      cell.font = Font(bold=True)
+        # Write headers for solar_df
+        solar_headers = solar_df.columns
+        for col_num, header in enumerate(solar_headers, start=1):
+            cell = ws2.cell(row=2, column=col_num, value=header)
+            cell.font = Font(bold=True)
 
-  # Write solar_df data
-  for row_num, (_, row_data) in enumerate(solar_df.iterrows(), start=3):
-      for col_num, value in enumerate(row_data, start=1):
-          ws2.cell(row=row_num, column=col_num, value=value)
+        # Write solar_df data
+        for row_num, (_, row_data) in enumerate(solar_df.iterrows(), start=3):
+            for col_num, value in enumerate(row_data, start=1):
+                ws2.cell(row=row_num, column=col_num, value=value)
 
-  # Write headers for non_solar_df
-  non_solar_headers = non_solar_df.columns
-  for col_num, header in enumerate(non_solar_headers, start=1):
-      cell = ws2.cell(row=len(solar_df) + 4, column=col_num, value=header)
-      cell.font = Font(bold=True)
+        # Write headers for non_solar_df
+        non_solar_headers = non_solar_df.columns
+        for col_num, header in enumerate(non_solar_headers, start=1):
+            cell = ws2.cell(row=len(solar_df) + 4, column=col_num, value=header)
+            cell.font = Font(bold=True)
 
-  # Write non_solar_df data
-  for row_num, (_, row_data) in enumerate(non_solar_df.iterrows(), start=len(solar_df) + 5):
-      for col_num, value in enumerate(row_data, start=1):
-          ws2.cell(row=row_num, column=col_num, value=value)
-  # Save the workbook
-  wb.save(filename)
-  st.write("Extracted data for SRPC_REA")
-
-fetch_data("2024", "jan")
+        # Write non_solar_df data
+        for row_num, (_, row_data) in enumerate(non_solar_df.iterrows(), start=len(solar_df) + 5):
+            for col_num, value in enumerate(row_data, start=1):
+                ws2.cell(row=row_num, column=col_num, value=value)
+        # Save the workbook
+        wb.save(filename)
+        st.write("Extracted data for SRPC_REA")
+      else:
+         print("No Data Found")
+         st.warning("No Data Found")
+         return
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 
 import WRPC_DSM_UI_Accounts
 import WRPC_REGIONAL_ENERGY_ACCOUNTS
+import SRPC_REA_RTA
 
 data_extracted = False
 
@@ -41,11 +42,11 @@ if __name__ == '__main__':
     function_mapping = {
         "WRPC Regional Accounts": WRPC_REGIONAL_ENERGY_ACCOUNTS.extract_data,
         "WRPC DSM UI Accounts": WRPC_DSM_UI_Accounts.fetch_pdfs,
-        # "SRPC_REA_REA": SRPC_REA_REA.,
+        "SRPC_REA_REA": SRPC_REA_RTA.fetch_data,
         # "SRPC Option 2": SRPC_WA_DSM
     }
     
-    checkbox_labels = ["WRPC Regional Accounts", "WRPC DSM UI Accounts"]
+    checkbox_labels = ["WRPC Regional Accounts", "WRPC DSM UI Accounts", "SRPC_REA_REA"]
     selected_option = st.radio("Select an option", checkbox_labels)
     # if st.button("Start"):
     function_mapping[selected_option](selected_year, selected_month)
