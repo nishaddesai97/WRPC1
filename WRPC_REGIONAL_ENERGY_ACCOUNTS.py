e58e6ab2db86ed31afbd7d8d290d2a65107a08c8
mishrasharad332 <163420422+mishrasharad332@users.noreply.github.com>
2024-03-22 11:26:38 +0530

    Update WRPC_REGIONAL_ENERGY_ACCOUNTS.py

--- WRPC_REGIONAL_ENERGY_ACCOUNTS.py
+++ WRPC_REGIONAL_ENERGY_ACCOUNTS.py
@@ -65,16 +65,12 @@ def search_text_in_pdf(title, url, search_text):
             # Find the line index containing the search text
             for idx, line in enumerate(lines):
                 if search_text in line:
-                    # print("search2")
-                    # Extract the entire row
-                    # found_row = ' '.join(lines[idx:idx]).strip()  # Adjusted to include next 3 lines
                     found_row = lines[idx]
-                    print("FOUND ROW ==", found_row)
                     break
             if found_row:
                 break  # Exit loop if the first occurrence is found
     pdf_document.close()
-    print("found_row", found_row)
+    # print("found_row", found_row)
     return found_row
 
 # Function to create clickable links in Excel
@@ -112,14 +108,12 @@ def search_text_in_multiple_pdfs(pdf_links, search_text, year):
         found_row = search_text_in_pdf(title, url, search_text)
         if found_row:
             all_rows.append((found_row, title, url))
-
     df = pd.concat([row_to_dataframe(row, title, url, year) for row, title, url in all_rows], ignore_index=True)
-    st.write(df)
+    # st.write(df)
     return df
 
 # Define a function to extract data based on selected year and title filter
 def extract_data(year, title_filter):
-    print("Extracting WRPC_Monthly Scheduled Revenue ")
     wrpc_base_url = "https://www.wrpc.gov.in"
     REA_link = f"{wrpc_base_url}/assets/data/REA_{year}.txt"
     response = requests.get(REA_link)
@@ -127,8 +121,8 @@ def extract_data(year, title_filter):
     pdf_title = ""
     pdf_url = ""
 
-    # print(response.status_code)
     if response.status_code == 200:
+        st.info("Extracting WRPC_Monthly Scheduled Revenue | Please Wait!")
         rea_data = response.text.split("\n")
         pdf_links = []
         for data_line in rea_data:
@@ -144,16 +138,13 @@ def extract_data(year, title_filter):
                     pdf_url = link
                     if title_filter.lower() in title.lower():
                         pdf_links.append((title, link))
-        
-        # print("pdf_links>>>>", pdf_links)
-                        
         # Search for the text in the multiple PDFs and append the results into one DataFrame
         df = search_text_in_multiple_pdfs(pdf_links, "Arinsun_RUMS", year)  # You can adjust the search text as needed
         
         sheet_name = 'WRPC_Monthly Scheduled Revenue'
         create_file(df, sheet_name)
-        st.write("Extracted WRPC_Monthly Scheduled Revenue")
-        # print(df)
+        print("Extracted WRPC_Monthly Scheduled Revenue✨")
+        st.success("Extracted WRPC_Monthly Scheduled Revenue✨")
     else:
         print("NO DATA FOUND FOR THE SPECIFIED DATE/ WRPC_Monthly Scheduled Revenue")
         st.warning("There's no data available for WRPC_Monthly Scheduled Revenue in the specified time frame.")
